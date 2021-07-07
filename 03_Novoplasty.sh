4bbf9a5d644b5a715470943c448e7d6fba622b2a
Gustavo A. Bravo <gustavo_bravo@fas.harvard.edu>
2021-07-07 13:21:52 -0400

    Basic information on required files to run NOVOPlasty

--- 03_Novoplasty.sh
+++ 03_Novoplasty.sh
@@ -12,4 +12,10 @@
 #SBATCH --mail-user=gustavo_bravo@fas.harvard.edu # Email to which notifications will be sent
 
 module load perl/5.26.1-fasrc01
+
+# Running NOVOPlasty requires having it in a local directory.
+# It also requires a config file (03_config.txt) and a batch file (03_batch.txt) that indicate the program how to proceed.
+# The config file contains informtion on the parameters ofthe assembly.
+# The batch file is necessary when running several samples at a time. It contains information on the reference used and the paths to the forward and reverse subsampled and clean read files.
+
 perl NOVOPlasty4.3.1.pl -c 03_config.txt
